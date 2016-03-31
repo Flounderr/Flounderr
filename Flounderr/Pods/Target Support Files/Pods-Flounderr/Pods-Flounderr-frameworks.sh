de05ad212f1090a90eab9c968ecfd048070e1259
Ha Nuel Lee <hanuelee@vt.edu>
2016-03-30 23:33:38 -0400

    Added Tesseract pod

--- Flounderr/Pods/Target Support Files/Pods-Flounderr/Pods-Flounderr-frameworks.sh
+++ Flounderr/Pods/Target Support Files/Pods-Flounderr/Pods-Flounderr-frameworks.sh
@@ -86,8 +86,10 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Flounderr/Bolts.framework"
   install_framework "Pods-Flounderr/Parse.framework"
+  install_framework "Pods-Flounderr/TesseractOCR.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Flounderr/Bolts.framework"
   install_framework "Pods-Flounderr/Parse.framework"
+  install_framework "Pods-Flounderr/TesseractOCR.framework"
 fi
