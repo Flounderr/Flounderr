250bbd5709b971ce6aa718680aaf54fdf4d852aa
Ha Nuel Lee <hanuelee@vt.edu>
2016-04-21 02:33:45 -0400

    Show events on calendar

--- Flounderr/Flounderr/CalendarViewController.swift
+++ Flounderr/Flounderr/CalendarViewController.swift
@@ -17,6 +17,7 @@ class CalendarViewController: UIViewController, CVCalendarViewDelegate, CVCalend
     @IBOutlet weak var logoutButton: UIBarButtonItem!
     
     var eventIndex: Int = 0
+    var eventList = NSMutableArray()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +25,35 @@ class CalendarViewController: UIViewController, CVCalendarViewDelegate, CVCalend
         // Do any additional setup after loading the view.
         menuView.delegate = self
         calendarView.delegate = self
+        
+        loadInitialEventData()
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         calendarView.commitCalendarViewUpdate()
         menuView.commitMenuViewUpdate()
+    }
+    func loadInitialEventData() {
+        if GoogleCalendarClient.sharedInstance.isUserAuthorized() {
+            GoogleCalendarClient.sharedInstance.fetchEvents({ (success: Bool) in
+                if success {
+                    for event in GoogleCalendarClient.sharedInstance.eventList {
+                        self.eventList.addObject(event)
+                    }
+                }
+                else {
+                    print("Fetching events not successful!")
+                }
+                
+                // Reload the calendar after fetching the events from GoogleCalendarClient
+                self.reloadCalendar()
+            })
+        }
+        else {
+            print("User doesn't have a google account!")
+        }
+
     }
     func presentationMode() -> CalendarMode {
         return .MonthView
@@ -38,40 +62,34 @@ class CalendarViewController: UIViewController, CVCalendarViewDelegate, CVCalend
         return .Sunday
     }
     func dotMarker(shouldShowOnDayView dayView: DayView) -> Bool {
-        return true
-        /*
-        if GoogleCalendarClient.sharedInstance.isUserAuthorized() {
-            GoogleCalendarClient.sharedInstance.fetchEvents({ (success: Bool) in
-                if success {
-                    //let dateFormatter = NSDateFormatter()
-                    //dateFormatter.dateFormat = "MM/dd/yyyy hh:mm"
-                    if GoogleCalendarClient.sharedInstance.eventList.count > 0 &&
-                        GoogleCalendarClient.sharedInstance.eventList.count > self.eventIndex {
-                        let event = GoogleCalendarClient.sharedInstance.eventList.objectAtIndex(self.eventIndex) as! NSDictionary
-                        if dayView.date.day == CVDate(date: event["date"] as! NSDate).day {
-                            print("\nDates match!!: ")
-                        }
-                        self.eventIndex++
-                    }
-                }
-                else {
-                    print("Fetching events not successful!")
-                }
-            })
+        
+        
+        if eventList.count > 0 && eventIndex < eventList.count && (dayView.date.day == CVDate(date: ((eventList.objectAtIndex(eventIndex) as! NSDictionary)["date"] as! NSDate)).day) {
+            eventIndex++
+            return true
         }
-        else {
-            print("User doesn't have a google account!")
-        }
-        */
+        return false
     }
     func dotMarker(colorOnDayView dayView: DayView) -> [UIColor] {
         return [UIColor.blueColor()]
+    }
+    func didShowNextMonthView(date: NSDate) {
+        reloadCalendar()
+    }
+    func didShowPreviousMonthView(date: NSDate) {
+        reloadCalendar()
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+    func reloadCalendar() {
+        eventIndex = 0
+        if let cc = self.calendarView.contentController as? CVCalendarMonthContentViewController {
+            cc.refreshPresentedMonth()
+        }
+        //print("reloadCalendar() called!")
+    }
     @IBAction func onLogout(sender: AnyObject) {
         print("Logout called!")
         PFUser.logOut()
